9584b7243ad2c828069e80beec590dd66778aaf7
Navjot singh <navjot8886@gmail.com>
2026-05-01 23:17:25 +0530

    Adding node lab 1

---
+++ 20-01-2026 nlab 1/app.js
@@ -0,0 +1,11 @@
+const data =require("./export.js");
+// const file =require("jjjjj");
+
+//import data from "./export"
+
+
+console.log("student details");
+console.log("ID",data.student.id);
+console.log("Name",data.student.name);
+console.log("Course",data.student.course);
+console.log("Marks",data.student.marks);
---
+++ 20-01-2026 nlab 1/export.js
@@ -0,0 +1,6 @@
+exports.student={
+    id : "1571",
+    name: "Navjot singh",
+    course: "B.tech",
+    marks: 80
+}
